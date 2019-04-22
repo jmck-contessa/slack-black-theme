e0c5e4d040b4370e16bf2cde8f3092a18b29b4c4
Josh McKearin <42548854+jmck-contessa@users.noreply.github.com>
2019-04-22 08:18:44 -0500

    remove lato font

--- ssb-interop.js
+++ ssb-interop.js
@@ -118,8 +118,7 @@ document.addEventListener("DOMContentLoaded", function() {
 }
 
 body {
-    font-family: 'Lato', sans-serif !important;
-    /* src: url('https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i'); */
+    src: url('https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i');
     text-rendering: optimizeLegibility important;
     word-spacing: -1px;
 }
